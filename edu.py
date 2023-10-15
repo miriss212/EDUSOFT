66330da9fadf4620f819432c274fc52b69158dae
Miriam Cidoríková <56067532+miriss212@users.noreply.github.com>
2023-10-15 15:42:31 +0200

    buttony s obrazkami, fonty

--- edu.py
+++ edu.py
@@ -1,7 +1,7 @@
 import tkinter as tk
-from tkinter import ttk
 from tkinter import filedialog
 from PIL import Image, ImageTk
+import tkinter.font as tkFont
 
 class MapEditor:
     def __init__(self, root):
@@ -10,7 +10,6 @@ class MapEditor:
 
         self.mode = "normal"  # Initial mode is normal
         
-
         # Open a JPEG image and convert it to PhotoImage
         self.background_image = self.load_and_resize_image("c:\\Users\\cidom\\OneDrive\\Dokumenty\\mAIN2\\edusoft\\sand.jpg", 400, 400)
 
@@ -18,7 +17,8 @@ class MapEditor:
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.background_image)  # Set the image as the background
         self.canvas.grid(row=1, column=0, columnspan=4)
 
-        self.mode_label = tk.Label(root, text="Mode: " + self.mode, bg="sandybrown")
+        custom_font = tkFont.Font(family="Lucida Sans Unicode", size=16, weight="bold", slant="italic")
+        self.mode_label = tk.Label(root, text="Mode: " + self.mode, bg="sandybrown", font=custom_font)
         self.mode_label.grid(row=0, column=0, columnspan=4)
 
         self.buttons_frame = tk.Frame(root)
@@ -27,12 +27,36 @@ class MapEditor:
         self.switch_button = tk.Button(self.buttons_frame, text="Switch Mode", command=self.toggle_mode, bg='sandybrown')
         self.switch_button.grid(row=0, column=0, padx=5, pady=5)
 
-        self.save_button = tk.Button(self.buttons_frame, text="Save Map", command=self.save_map, bg='sandybrown')
-        self.open_button = tk.Button(self.buttons_frame, text="Open Map", command=self.open_map, bg='sandybrown')
-        self.reset_button = tk.Button(self.buttons_frame, text="Reset", command=self.reset_map, bg='sandybrown')
-        self.try_button = tk.Button(self.buttons_frame, text="Try Map", command=self.try_map, bg='sandybrown')
-        self.no_solution_button = tk.Button(self.buttons_frame, text="No Solution", command=self.no_solution, bg='sandybrown')
+        img1 = Image.open("C:\\Users\\cidom\\OneDrive\\Dokumenty\\mAIN2\\edusoft\\save.png")
+        img1 = img1.resize((30, 30), Image.NEAREST)
+        photo1 = ImageTk.PhotoImage(img1)
 
+        img_reset= Image.open("C:\\Users\\cidom\\OneDrive\\Dokumenty\\mAIN2\\edusoft\\reset.png")
+        img_reset = img_reset.resize((30, 30), Image.NEAREST)
+        photo_reset = ImageTk.PhotoImage(img_reset)
+
+        img_open = Image.open("C:\\Users\\cidom\\OneDrive\\Dokumenty\\mAIN2\\edusoft\\open.png")
+        img_open =  img_open.resize((30, 30), Image.NEAREST)
+        photo_open = ImageTk.PhotoImage(img_open)
+
+        img_find = Image.open("C:\\Users\\cidom\\OneDrive\\Dokumenty\\mAIN2\\edusoft\\solution.png")
+        img_find  =  img_find .resize((30, 30), Image.NEAREST)
+        photo_find = ImageTk.PhotoImage(img_find)
+
+        img_no_sol = Image.open("C:\\Users\\cidom\\OneDrive\\Dokumenty\\mAIN2\\edusoft\\no_sol.png")
+        img_no_sol =  img_no_sol.resize((30, 30), Image.NEAREST)
+        photo_no_sol = ImageTk.PhotoImage(img_no_sol)
+
+        self.save_button = tk.Button(self.buttons_frame, text="Save Map",image=photo1, command=self.save_map)
+        self.save_button.image = photo1
+        self.open_button = tk.Button(self.buttons_frame, text="Open Map", command=self.open_map, image=photo_open)
+        self.open_button.image = photo_open
+        self.reset_button = tk.Button(self.buttons_frame, text="Reset", command=self.reset_map, image=photo_reset)
+        self.reset_button.image = photo_reset
+        self.try_button = tk.Button(self.buttons_frame, text="Try Map", command=self.try_map, image=photo_find)
+        self.try_button.image = photo_find
+        self.no_solution_button = tk.Button(self.buttons_frame, text="No Solution", command=self.no_solution, image = photo_no_sol)
+        self.no_solution_button.image = photo_no_sol
         self.save_button.grid(row=0, column=1, padx=5, pady=5)
         self.open_button.grid(row=0, column=2, padx=5, pady=5)
         self.reset_button.grid(row=0, column=3, padx=5, pady=5)
@@ -79,7 +103,12 @@ class MapEditor:
         self.size_slider.set(400)
         self.size_label.grid(row=1, column=1, padx=5, pady=5)
         self.size_slider.grid(row=1, column=2, padx=5, pady=5)
-        self.submit_resize_button = tk.Button(self.buttons_frame, text="Submit Resize", command=self.submit_resize, bg='sandybrown')
+
+        img2 = Image.open("C:\\Users\\cidom\\OneDrive\\Dokumenty\\mAIN2\\edusoft\\submit.png")
+        img2 = img2.resize((30, 30), Image.NEAREST)
+        photo2 = ImageTk.PhotoImage(img2)
+        self.submit_resize_button = tk.Button(self.buttons_frame, text="Submit Resize", command=self.submit_resize, image = photo2)
+        self.submit_resize_button.image = photo2
 
     def submit_resize(self):
         new_size = self.size_slider.get()
@@ -114,3 +143,4 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = MapEditor(root)
     root.mainloop()
+    
